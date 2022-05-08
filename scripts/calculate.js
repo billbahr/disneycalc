fba34270e0f096c5e8212201d6aaf4230aaad802
Bill Bahr <78818242+billbahr@users.noreply.github.com>
2022-05-08 16:08:24 -0500

    Refactor for better mobile display

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,6 +1,7 @@
 var DateTime = luxon.DateTime;
 const tripDate = document.querySelector('.travel-date');
 const diningDate = document.querySelector('.dining-date');
+const parkDate = document.querySelector('.park-date');
 const dvc7Date = document.querySelector('.dvc7-date');
 const dvc11Date = document.querySelector('.dvc11-date');
 
@@ -10,6 +11,9 @@ function calcDates(pageDate) {
     
     // Dining reservations can be made 60 days before
     diningDate.innerHTML = beginDate.minus({days:60}).toLocaleString(DateTime.DATE_HUGE).replaceAll(',',' ');
+
+    // Disneyland park reservations can be made 120 days before
+    parkDate.innerHTML = beginDate.minus({days:120}).toLocaleString(DateTime.DATE_HUGE).replaceAll(',',' ');
 
     // Home resort DVC reservations can be made 11 months before
     dvc11Date.innerHTML = beginDate.minus({months:11}).toLocaleString(DateTime.DATE_HUGE).replaceAll(',',' ');
